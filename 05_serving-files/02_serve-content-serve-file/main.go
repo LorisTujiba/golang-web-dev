bbb2a1a1091fe20b62f4d895ef23be8629c9c596
Loris Tujiba Soejonopoetro <pisceslrz@gmail.com>
2017-07-06 16:47:20 +0700

    added serving file material

---
+++ 05_serving-files/02_serve-content-serve-file/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"io"
+)
+
+/*
+Let's  serve an image using the serve file command
+ */
+
+func notBeingServedFromOurServer(w http.ResponseWriter,r *http.Request){
+
+	w.Header().Set("Content-Type","text/html;charset=utf-8")
+	io.WriteString(w,`<img src="loris.jpg">`)//not from our server, served from somewhere
+}
+
+func handle(w http.ResponseWriter, r *http.Request){
+
+	fl,err := os.Open("loris.jpg")
+	if err!=nil{
+		http.Error(w,"File Not Found",404)//returns http error 404
+		return
+	}
+	defer fl.Close()
+
+	fi,err := fl.Stat()//get the attributes data
+	if err!=nil{
+		http.Error(w,"File Not Found",404)//returns http error 404
+		return
+	}
+
+	http.ServeContent(w,r,fi.Name(),fi.ModTime(),fl)//so we can access the name, and the last modified time
+	http.ServeFile(w,r,"loris.jpg")//much simpler with this
+
+}
+
+func main(){
+
+	http.HandleFunc("/",notBeingServedFromOurServer)
+	http.HandleFunc("/loris.jpg",handle)
+	http.ListenAndServe(":8080",nil)
+
+}
---
+++ 05_serving-files/05_static-file/main.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+Static file server for static website
+
+This code below will show the dir
+
+IF, we have file with the name of index of html, it will no longer shows the files on
+the dir
+
+commonly in static website, there has to be an index.html file
+
+*/
+
+import "net/http"
+
+func main(){
+	http.ListenAndServe(":8080",http.FileServer(http.Dir(".")))
+}
